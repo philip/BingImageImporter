216b7668bae1dd91c7b4b57907cea56db21a6871
philip <philip@roshambo.org>
2016-07-27 16:19:30 -0700

    Added an example to honor happy cats

---
+++ examples/cat.php
@@ -0,0 +1,19 @@
+<?php
+require "../lib/BingImageImporter.php";
+
+$bing = new BingImageImporter();
+
+// Your Key
+$bing->setApiKey("YOUR KEY HERE");
+
+// We'll search for a 'Happy Cat' image
+$bing->setQuery("Happy Cat");
+
+// Get image info, by default a random image
+$image = $bing->getImageInfo();
+
+// Example output
+echo '<img src="'.  $image['url'] . '" width="'. $image['width'] . '" height="'. $image['height'] . '"/>' . PHP_EOL;
+
+// See what information is available
+print_r($image);
